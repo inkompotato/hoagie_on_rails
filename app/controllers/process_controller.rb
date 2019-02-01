6ef2f506adb2ec252767e1b725f684afc687aa6e
inkompotato <fra.js.1998@gmail.com>
2019-02-01 15:19:15 +0100

    bugfixes

--- app/controllers/process_controller.rb
+++ app/controllers/process_controller.rb
@@ -1,9 +1,31 @@
 class ProcessController < ApplicationController
+  # resources :items
+  # resources :orders
+  # resources :customers
   def new
-    @process = Order.new, Item.new
+    @order = Order.new
+    @customer = Customer.new
+    @item = Item.new
+  end
+
+  def index
+    @items = Item.all
+  end
+
+  def show
   end
 
   def create
+    @item = Item.new(item_params)
 
+    respond_to do |format|
+      if @item.save
+        # format.html { redirect_to @item, notice: 'Item was successfully created.'}
+        format.json { render :show, status: :created, location: @item }
+      else
+        # format.html { render :new }
+        format.json { render json: @item.errors, status: :unprocessable_entity }
+      end
+    end
   end
 end
