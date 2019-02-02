d4543e2530d030924dfdd6ee06b20b43614361ab
inkompotato <fra.js.1998@gmail.com>
2019-02-02 15:38:16 +0100

    added price calculation logic

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,19 @@
 class Item < ApplicationRecord
   has_many :ingredients
   belongs_to :order
+
+  def price
+    price = 350
+
+    if name != nil
+      price += Ingredient.find(main_id).price
+      price += Ingredient.find(second_id).price
+      price += Ingredient.find(salad_id).price
+      price += Ingredient.find(cheese_id).price
+      price += Ingredient.find(sauce_id).price
+      price += Ingredient.find(drink_id).price
+    end
+
+    self.price = price
+  end
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,3 +1,17 @@
 class Order < ApplicationRecord
   belongs_to :customer
+
+  def items_list
+    Item.where(order_id: id)
+  end
+
+  def price
+    # hoagies = Item.where('order_id = id')
+    price = 0
+    items_list.each do |h|
+      price += h.price
+    end
+    self.price = price
+  end
 end
+
